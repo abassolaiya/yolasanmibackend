757472b5fec20e4ad2ee4ed071d9df93b93b041c
Abass Olaiya <ayotundeolaiya@gmail.com>
2024-08-12 04:52:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path"); 
 
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
